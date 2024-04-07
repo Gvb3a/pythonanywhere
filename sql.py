289c7257bc58528a8a3b2638a855a5ba583fcfca
Boris <135196754+Gvb3a@users.noreply.github.com>
2024-04-07 13:54:24 +0300

    Add files via upload

---
+++ sql.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+
+def sql_launch():
+    connection = sqlite3.connect('pythonanywhere.db')
+    cursor = connection.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS user (
+        id INT,
+        username TEXT,
+        token TEXT
+        )
+        ''')
+    connection.commit()
+    connection.close()
+
+
+def sql_token_and_username(user_id):
+    connection = sqlite3.connect('pythonanywhere.db')
+    cursor = connection.cursor()
+
+    cursor.execute(f"SELECT * FROM user WHERE id = {user_id}")
+    row = cursor.fetchone()
+    connection.close()
+
+    if row is None or row[2] == 'None':
+        return ['None', 'None']
+    else:
+        return row[1], row[2]
+
+
+def sql_change(user_id, username, token):
+    connection = sqlite3.connect('pythonanywhere.db')
+    cursor = connection.cursor()
+
+    cursor.execute(f"SELECT * FROM user WHERE id = {user_id}")
+    row = cursor.fetchone()
+
+    if row is None:
+        cursor.execute(f"INSERT INTO user(id, username, token) VALUES ({user_id}, '{username}', '{token}')")
+    else:
+        cursor.execute(f"UPDATE user SET (username, token) = ('{username}', '{token}') WHERE id = {user_id}")
+
+    connection.commit()
+    connection.close()
