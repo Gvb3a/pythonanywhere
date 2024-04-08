1c760f368b2751dc9595fe58128a87131ebc63ea
Boris <135196754+Gvb3a@users.noreply.github.com>
2024-04-08 21:47:13 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+bot_token = 'bot_token'  # https://t.me/BotFather
+
+start_message = ('[Github](https://github.com/Gvb3a/pythonanywhere) of the project.\n'
+                 'This telegram bot provides convenient access to all the features of the '
+                 '[PythonAnywhere API](https://help.pythonanywhere.com/pages/API/). '
+                 'But to use it you need to specify your username and '
+                 '[token](https://www.pythonanywhere.com/account/#api_token).')
