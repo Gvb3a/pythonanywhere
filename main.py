61290474a44b4d3fc8cb19ebc5c044707b24b6d0
Boris <135196754+Gvb3a@users.noreply.github.com>
2024-04-08 21:47:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,149 +1,85 @@
 import requests
-import sqlite3
-import json
-
 import datetime
 
-from colorama import init, Fore, Style
 from aiogram import Bot, Dispatcher, types, F
-from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.filters import Command, CommandStart, StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import default_state, State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.session.aiohttp import AiohttpSession
 
+
 from sql import sql_launch, sql_change, sql_token_and_username
-init()
+from function import cpu, consoles_info, always_on_info
+from config import bot_token, start_message
 
 # session = AiohttpSession(proxy="http://proxy.server:3128")
-bot_token = 'bot_token'
 bot = Bot(bot_token)  # bot = Bot(bot_token, session=session)
-dp = Dispatcher()
-fsm_by_lazy = []
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
 
 
-def name_fuc(username, name):
-    return username if username is not None else name
+class FSMFillForm(StatesGroup):
+    fill_change = State()
 
 
-def cpu(username, token):
-
-    response = requests.get(f'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/',
-                                headers={'Authorization': f'Token {token}'})
-    if response.status_code == 200:
-        parsed_data = json.loads(response.content)
-
-        cpu_limit = parsed_data['daily_cpu_limit_seconds']
-        cpu_usage = parsed_data['daily_cpu_total_usage_seconds']
-        reset_time = parsed_data['next_reset_time']
-
-        cpu_percent = (cpu_usage / cpu_limit) * 100
-
-        reset_datetime = datetime.datetime.fromisoformat(reset_time.replace('T', ' ')).replace(
-                tzinfo=datetime.timezone.utc)
-        now = datetime.datetime.now(datetime.timezone.utc)
-        time_until_reset = reset_datetime - now
-
-        hours = int(time_until_reset.total_seconds() // 3600)
-        minutes = int((time_until_reset.total_seconds() % 3600) // 60)
-
-        if hours != 0:
-            time = f'{hours} hours, {minutes} minutes'
-        else:
-            time = f'{minutes} minutes'
-
-        return f"{cpu_percent:.0f}% used – {cpu_usage:.2f}s of {cpu_limit}s. Resets in {time}"
-
-    else:
-        return 'error'
+def print_fuc(message, name):
+    print(f'{message} by {name} at {datetime.datetime.now().strftime("%H:%M:%S")}')
 
 
-def consoles_info(username, token):
-    response = requests.get(
-        f'https://www.pythonanywhere.com/api/v0/user/{username}/consoles/',
-        headers={'Authorization': f'Token {token}'})
-
-    if response.status_code == 200:
-        parsed_data = json.loads(response.content)
-        result = ''
-
-        if len(parsed_data) == 0:
-            return 'You have no consoles.', False
-
-        inline_list = []
-        for console in parsed_data:
-            name = console['name']
-            console_id = console['id']
-            console_url = 'https://www.pythonanywhere.com' + console['console_url']
-            result += f'\n[{name}]({console_url}) - `{console_id}`'
-            inline_list.append(InlineKeyboardButton(text=name,
-                                                    callback_data=f'consoles-{console_id}'))
-
-        return result, inline_list
-    else:
-        return 'error', False
-
-
-def always_on_info(username, token):
-    response = requests.get(
-        f'https://www.pythonanywhere.com/api/v0/user/{username}/always_on/',
-        headers={'Authorization': f'Token {token}'})
-
-    if response.status_code == 200:
-        parsed_data = json.loads(response.content)
-        result = ''
-
-        if len(parsed_data) == 0:
-            return '\nYou have no tasks yet.', False
-
-        inline_list = []
-        for always_on in parsed_data:
-            command = always_on['command']
-            description = always_on['description']
-            always_on_id = always_on['id']
-            url = 'https://www.pythonanywhere.com' + always_on['url']
-            result += f'\n[{command}]({url})({description}) - `{always_on_id}`'
-            inline_list.append(InlineKeyboardButton(text=command,
-                                                    callback_data=f'always_on-{always_on_id}'))
-
-        return result, inline_list
-    else:
-        return '\nerror', False
-
-
-
-@dp.message(CommandStart())
+@dp.message(CommandStart())  # start command processing
 async def command_start_handler(message: Message) -> None:
-    name = name_fuc(message.from_user.username, message.from_user.full_name)
-    await message.answer(f'Hello, {name}')
-    print(f'start by {message.from_user.username}')
+    name = f'{message.from_user.full_name}({message.from_user.username})'
+    await message.answer(f'Hello, {message.from_user.full_name}. \n{start_message}', parse_mode='Markdown')  # send a message
+    print_fuc('/start', name)  # output the information that there was a request to the console
 
 
 @dp.message(Command('setting'))
 async def command_setting(message: Message) -> None:
     user_id = message.from_user.id
+    name = f'{message.from_user.full_name}({message.from_user.username})'
     username, token = sql_token_and_username(user_id)
-    await bot.send_message(user_id, text='This is your settings. You need to provide your username and API token to retrieve the data.' 
+    await bot.send_message(user_id, text=
+                         'This is your settings. You need to provide your username and API token to retrieve the data.' 
                          f'\nid: {user_id}' 
                          f'\nusername: {username}'
                          f'\ntoken: `{token}`\n\n'
-                         f'To change username and token, send the /change command. If you want to delete the data, send the /deletedata command', parse_mode='Markdown')
-    print(f'setting by {message.from_user.username}')
+                         f'To change username and token, send the /change command. If you want to delete the data, '
+                         f'send the /deletedata command', parse_mode='Markdown')
+    print_fuc('/setting', name)
 
 
 @dp.message(Command('change'))
-async def command_change(message: Message) -> None:
+async def command_change(message: Message, state: FSMContext) -> None:
+    name = f'{message.from_user.full_name}({message.from_user.username})'
     await message.answer('Send your username and token. Please use this format:\nusername - token')
-    global fsm_by_lazy
-    fsm_by_lazy.append(message.from_user.id)
-    print(f'change by {message.from_user.username}')
+    await state.set_state(FSMFillForm.fill_change)
+    print_fuc('/change', name)
 
 
 @dp.message(Command('deletedata'))
 async def command_delete_data(message: Message) -> None:
+    name = f'{message.from_user.full_name}({message.from_user.username})'
     user_id = message.from_user.id
     sql_change(user_id, None, None)
     await message.answer('The data has been deleted')
-    print(f'deletedata by {message.from_user.username}')
+    print_fuc('/deletedata', name)
 
+
+@dp.message(Command('cancel'), StateFilter(default_state))
+async def command_cancel_default_state(message: Message):
+    name = f'{message.from_user.full_name}({message.from_user.username})'
+    await message.answer('There\'s nothing to cancel. You\'re outside the state machine')
+    print_fuc('/cancel(nothing to cancel)', name)
+
+
+@dp.message(Command('cancel'), ~StateFilter(default_state))
+async def command_cancel(message: Message, state: FSMContext):
+    name = f'{message.from_user.full_name}({message.from_user.username})'
+    await message.answer('You got out of the state machine')
+    await state.clear()
+    print_fuc('/cancel(got out)', name)
 
 @dp.message(Command('consoles'))
 async def command_consoles(message: Message) -> None:
@@ -153,10 +89,10 @@ async def command_consoles(message: Message) -> None:
 @dp.callback_query(F.data)
 async def callback_data(callback: types.CallbackQuery):
     data = callback.data
+    name = f'{callback.from_user.full_name}({callback.from_user.username})'
     user_id = callback.from_user.id
 
     if data[:6] == 'update':
-        from random import randint
         username, token = sql_token_and_username(user_id)
 
         cpu_result = cpu(username, token)
@@ -183,26 +119,31 @@ async def callback_data(callback: types.CallbackQuery):
         data = data[9:]
 
     await callback.answer()
+    print_fuc(f'callback {data}', name)
 
 
-@dp.message()
-async def main_handler(message: types.Message) -> None:
-    print(f'message by {message.from_user.username}')
-    global fsm_by_lazy
+@dp.message(StateFilter(FSMFillForm.fill_change))
+async def process_name_sent(message: Message, state: FSMContext):
+    name = f'{message.from_user.full_name}({message.from_user.username})'
+    username, token = message.text.split(' - ')
     user_id = message.from_user.id
-    if user_id in fsm_by_lazy:
-        username, token = message.text.split(' - ')
-        response = requests.get(
-            f'https://www.pythonanywhere.com/api/v0/user/{username}/consoles/',
-            headers={'Authorization': f'Token {token}'})
+    response = requests.get(
+        f'https://www.pythonanywhere.com/api/v0/user/{username}/consoles/',
+        headers={'Authorization': f'Token {token}'})
 
-        if response.status_code == 200:
-            sql_change(user_id, username, token)
-            await bot.send_message(user_id, 'Data saved')
-        else:
-            await bot.send_message(user_id, 'Incorrect input')
-        fsm_by_lazy.remove(user_id)
-        return
+    if response.status_code == 200:
+        sql_change(user_id, username, token)
+        await bot.send_message(user_id, 'Data saved')
+    else:
+        await bot.send_message(user_id, 'Incorrect input')
+    await state.clear()
+    print_fuc('message(change)', name)
+
+
+@dp.message(StateFilter(default_state))
+async def main_handler(message: types.Message) -> None:
+    name = f'{message.from_user.full_name}({message.from_user.username})'
+    user_id = message.from_user.id
 
     username, token = sql_token_and_username(user_id)
 
@@ -225,11 +166,11 @@ async def main_handler(message: types.Message) -> None:
     else:
         await bot.send_message(user_id, 'Error')
 
+    print_fuc('message', name)
 
 
 
 if __name__ == '__main__':
     sql_launch()
-    print(f'The bot {Fore.RED}launches{Style.RESET_ALL} at {datetime.datetime.now().strftime("%H:%M:%S %d.%m.%Y")}')
+    print(f'The bot launches at {datetime.datetime.now().strftime("%H:%M:%S %d.%m.%Y")}')
     dp.run_polling(bot)
-    
